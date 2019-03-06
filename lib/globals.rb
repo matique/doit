bbb2776c39ca5b462ebb8d6f12490877a43e76b3
matique <dittmar.krall@matique.de>
2019-03-06 12:09:04 +0100

    'v0.3.6'

--- lib/globals.rb
+++ lib/globals.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 module Globals
-#  VERSION = '0.3.6'     # 2019-03-06
-  VERSION = '0.3.5'     # 2018-09-11
+  VERSION = '0.3.6'     # 2019-03-06
+#  VERSION = '0.3.5'     # 2018-09-11
 #  VERSION = '0.3.4'     # 2018-08-19
 #  VERSION = '0.3.3'
 end
